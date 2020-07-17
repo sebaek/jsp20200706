d2f31492f0681ffe9dec51f7125ab975103b39c5
sebaek <sebaek@gmail.com>
2020-07-17 10:33:06 +0900

    filter

--- src/chap19/sample/LoginFilterEx1Servlet.java
+++ src/chap19/sample/LoginFilterEx1Servlet.java
@@ -1,11 +1,14 @@
 package chap19.sample;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class LoginFilterEx1Servlet
@@ -26,16 +29,27 @@ public class LoginFilterEx1Servlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("login jsp");
+		
+		request
+		.getRequestDispatcher("/WEB-INF/chap19/login.jsp")
+		.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		session.setAttribute("user", request.getParameter("id"));
+		
 		doGet(request, response);
 	}
 
 }
+
+
+
+
+
+
+
