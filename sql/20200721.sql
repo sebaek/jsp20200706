ff822ef58b26673aafbe7279ad5382d7a68e961b
sebaek <sebaek@gmail.com>
2020-07-21 12:28:26 +0900

    dbcp

--- sql/20200721.sql
+++ sql/20200721.sql
@@ -15,4 +15,35 @@ select * from member;
 insert into member
 values ('홍길동', 35);
 
+alter table member add column
+(
+id int auto_increment primary key
+);
+
+select * from member;
+
+create table money (
+id int auto_increment primary key,
+name varchar(255),
+money int
+);
+
+insert into money (name, money) values 
+('john', 1000);
+insert into money (name, money) values
+('jane', 1000);
+
+update money set money = 
+(money - 10)
+where id=1;
+
+update money set money = 
+(money + 10)
+where id=2;
+select * from money;
+
+
+
+
+
 
--- src/chap20/MyAppListener.java
+++ src/chap20/MyAppListener.java
@@ -1,9 +1,19 @@
 package chap20;
 
+import java.sql.DriverManager;
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+
+import org.apache.commons.dbcp2.ConnectionFactory;
+import org.apache.commons.dbcp2.DriverManagerConnectionFactory;
+import org.apache.commons.dbcp2.PoolableConnection;
+import org.apache.commons.dbcp2.PoolableConnectionFactory;
+import org.apache.commons.dbcp2.PoolingDriver;
+import org.apache.commons.pool2.impl.GenericObjectPool;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 
 /**
  * Application Lifecycle Listener implementation class MyAppListener
@@ -37,8 +47,52 @@ public class MyAppListener implements ServletContextListener {
 		
 		System.out.println(app.getInitParameter("logEnabled"));
 		System.out.println(app.getInitParameter("debugLevel"));
+		
+		initConnectionPool();
 	}
+
+	private void initConnectionPool() {
+		try {
+			String jdbcUrl = 
+					"jdbc:mysql://localhost/test1" +
+					"?serverTimezone=Asia/Seoul";
+			String username = "root";
+			String pw = "rootpw";
+			
+			ConnectionFactory connFactory = 
+				new DriverManagerConnectionFactory(jdbcUrl, username, pw);
+			
+			PoolableConnectionFactory pcf = 
+				new PoolableConnectionFactory(connFactory, null);
+			pcf.setValidationQuery("SELECT 1");
+			
+			GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
+			poolConfig.setTimeBetweenEvictionRunsMillis(1000 * 60 * 5);
+			poolConfig.setTestWhileIdle(true);
+			poolConfig.setMinIdle(4);
+			poolConfig.setMaxTotal(50);
+			
+			GenericObjectPool<PoolableConnection> connectionPool = 
+				new GenericObjectPool<>(pcf, poolConfig);
+			pcf.setPool(connectionPool);
+			
+			Class.forName("org.apache.commons.dbcp2.PoolingDriver");
+			PoolingDriver driver = (PoolingDriver)
+				DriverManager.getDriver("jdbc:apache:commons:dbcp:");
+			driver.registerPool("test1", connectionPool);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+	
+	
 }
+
+
+
+
+
 
 
 
