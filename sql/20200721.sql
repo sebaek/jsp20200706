6db2501130f90983d3cd83e4e6962cae96b6af5f
sebaek <sebaek@gmail.com>
2020-07-21 09:53:26 +0900

    jdbc

---
+++ sql/20200721.sql
@@ -0,0 +1,18 @@
+create database test1;
+
+use test1;
+
+create table member (
+	name varchar(255),
+    age int
+);
+
+insert into member
+values ('john', 33);
+
+select * from member;
+
+insert into member
+values ('홍길동', 35);
+
+
